ea857fe6ed6d04c1ad09e1480ba53ccb038b4fb5
osvalcde <osvalcde@gmail.com>
2023-03-17 16:38:46 -0300

    Add files via upload

--- odonto.sql
+++ odonto.sql
@@ -5,6 +5,9 @@ SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
 
 -- -----------------------------------------------------
+-- Schema mydb
+-- -----------------------------------------------------
+-- -----------------------------------------------------
 -- Schema odonto
 -- -----------------------------------------------------
 DROP SCHEMA IF EXISTS `odonto` ;
@@ -20,14 +23,14 @@ USE `odonto` ;
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `odonto`.`doctores` (
   `id` INT NOT NULL,
-  `nombre` VARCHAR(45) NULL,
-  `apellido` VARCHAR(45) NULL,
-  `ruc` VARCHAR(45) NULL,
-  `telefono` VARCHAR(45) NULL,
-  `observacion` VARCHAR(45) NULL,
-  `correo` VARCHAR(45) NULL,
-  `created_at` DATETIME NULL DEFAULT NOW(),
-  `updated_at` DATETIME NULL DEFAULT NOW(),
+  `nombre` VARCHAR(45) NULL DEFAULT NULL,
+  `apellido` VARCHAR(45) NULL DEFAULT NULL,
+  `ruc` VARCHAR(45) NULL DEFAULT NULL,
+  `telefono` VARCHAR(45) NULL DEFAULT NULL,
+  `observacion` VARCHAR(45) NULL DEFAULT NULL,
+  `email` VARCHAR(45) NULL DEFAULT NULL,
+  `password` VARCHAR(255) NULL,
+  `created_at` DATETIME NULL DEFAULT CURRENT_TIMESTAMP(),
   PRIMARY KEY (`id`))
 ENGINE = InnoDB;
 
@@ -37,16 +40,16 @@ ENGINE = InnoDB;
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `odonto`.`pacientes` (
   `id` INT NOT NULL AUTO_INCREMENT,
-  `nombre` VARCHAR(45) NULL,
-  `apellido` VARCHAR(45) NULL,
-  `ci_ruc` VARCHAR(20) NULL,
-  `direccion` VARCHAR(150) NULL,
-  `nro_telefono` VARCHAR(45) NULL,
-  `fecha_nacimiento` DATE NULL,
-  `sexo` VARCHAR(10) NULL,
-  `patologia` VARCHAR(150) NULL,
-  `created_at` DATETIME NULL DEFAULT NOW(),
-  `updated_at` DATETIME NULL DEFAULT NOW(),
+  `nombre` VARCHAR(45) NULL DEFAULT NULL,
+  `apellido` VARCHAR(45) NULL DEFAULT NULL,
+  `ci_ruc` VARCHAR(20) NULL DEFAULT NULL,
+  `direccion` VARCHAR(150) NULL DEFAULT NULL,
+  `nro_telefono` VARCHAR(45) NULL DEFAULT NULL,
+  `fecha_nacimiento` DATE NULL DEFAULT NULL,
+  `sexo` VARCHAR(10) NULL DEFAULT NULL,
+  `patologia` VARCHAR(150) NULL DEFAULT NULL,
+  `created_at` DATETIME NULL DEFAULT CURRENT_TIMESTAMP(),
+  `updated_at` DATETIME NULL DEFAULT CURRENT_TIMESTAMP(),
   `id_doctor` INT NOT NULL,
   PRIMARY KEY (`id`),
   INDEX `fk_pacientes_doctores1_idx` (`id_doctor` ASC) VISIBLE,
@@ -63,12 +66,12 @@ ENGINE = InnoDB;
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `odonto`.`consultas` (
   `id` INT NOT NULL AUTO_INCREMENT,
-  `fecha_hora` DATETIME NULL,
-  `comentario` VARCHAR(255) NULL,
-  `pago_consulta` INT NULL,
-  `pago_tratamiento` INT NULL,
-  `created_at` DATETIME NULL DEFAULT NOW(),
-  `updated_at` DATETIME NULL DEFAULT NOW(),
+  `fecha_hora` DATETIME NULL DEFAULT NULL,
+  `comentario` VARCHAR(255) NULL DEFAULT NULL,
+  `pago_consulta` INT NULL DEFAULT NULL,
+  `pago_tratamiento` INT NULL DEFAULT NULL,
+  `created_at` DATETIME NULL DEFAULT CURRENT_TIMESTAMP(),
+  `updated_at` DATETIME NULL DEFAULT CURRENT_TIMESTAMP(),
   `id_paciente` INT NOT NULL,
   `id_doctor` INT NOT NULL,
   PRIMARY KEY (`id`),
@@ -92,8 +95,8 @@ ENGINE = InnoDB;
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `odonto`.`tratamientos` (
   `id` INT NOT NULL AUTO_INCREMENT,
-  `nombre` VARCHAR(45) NULL,
-  `observacion` VARCHAR(255) NULL,
+  `nombre` VARCHAR(45) NULL DEFAULT NULL,
+  `observacion` VARCHAR(255) NULL DEFAULT NULL,
   PRIMARY KEY (`id`))
 ENGINE = InnoDB;
 
@@ -103,8 +106,8 @@ ENGINE = InnoDB;
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `odonto`.`paciente_trata` (
   `id` INT NOT NULL AUTO_INCREMENT,
-  `costo_total` INT NULL,
-  `updated_at` DATETIME NULL DEFAULT NOW(),
+  `costo_total` INT NULL DEFAULT NULL,
+  `updated_at` DATETIME NULL DEFAULT CURRENT_TIMESTAMP(),
   `id_paciente` INT NOT NULL,
   `id_tratamiento` INT NOT NULL,
   PRIMARY KEY (`id`),
@@ -128,10 +131,10 @@ ENGINE = InnoDB;
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `odonto`.`gastos` (
   `id` INT NOT NULL,
-  `concepto` VARCHAR(45) NULL,
-  `monto` INT NULL,
-  `fecha` DATE NULL,
-  `updated_at` DATETIME NULL DEFAULT NOW(),
+  `concepto` VARCHAR(45) NULL DEFAULT NULL,
+  `monto` INT NULL DEFAULT NULL,
+  `fecha` DATE NULL DEFAULT NULL,
+  `updated_at` DATETIME NULL DEFAULT CURRENT_TIMESTAMP(),
   `id_doctor` INT NOT NULL,
   PRIMARY KEY (`id`),
   INDEX `fk_gastos_doctores1_idx` (`id_doctor` ASC) VISIBLE,
