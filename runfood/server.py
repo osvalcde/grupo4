46c9ec8fe24c08976e966fa2c11f53f171185e56
GermanLat <112091575+GermanLat@users.noreply.github.com>
2023-03-18 20:32:25 -0300

    Incluir controlador de doctores

--- runfood/server.py
+++ runfood/server.py
@@ -1,5 +1,5 @@
 from flask_app import app
-from flask_app.controllers import clietes, pacientes, transactions
+from flask_app.controllers import doctores, pacientes, transactions
 
 if __name__=='__main__':
     app.run(debug=True)
