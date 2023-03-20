94626e151ada7ad97ea7117e1704e055a92892b5
GermanLat <112091575+GermanLat@users.noreply.github.com>
2023-03-19 23:15:22 -0300

    Controlador de consultas

--- runfood/server.py
+++ runfood/server.py
@@ -1,5 +1,5 @@
 from flask_app import app
-from flask_app.controllers import doctores, pacientes, transactions
+from flask_app.controllers import doctores, pacientes, transactions, consultas
 
 if __name__=='__main__':
     app.run(debug=True)
